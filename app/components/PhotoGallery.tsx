09363251c5c61d2d0dddaa3b18c94e5f9939538b
LorenzoDF13 <difilippolorenzo0@gmail.com>
2023-10-06 17:03:11 +0200

    fixed bgOpacity and same things on home page

--- app/components/PhotoGallery.tsx
+++ app/components/PhotoGallery.tsx
@@ -30,7 +30,7 @@ const hiddenImages: {
 ];
 const PhotoGallery = (props: Props) => {
   return (
-    <Gallery>
+    <Gallery options={{ bgOpacity: 1 }}>
       <div>
         <div className="flex gap-4 m-auto flex-col md:flex-row  max-w-screen-lg px-4 mx-auto">
           <div className=" md:max-w-2/3 w-full hover:bg-neutral-400 hover:opacity-50  relative  hover:text-base-300  cursor-pointer transition-all rounded-md overflow-hidden ">
--- app/page.tsx
+++ app/page.tsx
@@ -13,22 +13,47 @@ console.log(pages);
 const page = (props: Props) => {
   return (
     <div className="max-w-screen-lg px-5 m-auto">
-      <div className="card w-96 card-compact sm:card-normal bg-base-100 shadow-xl">
-        <Link href={"/green-marine"}>
-          <figure>
-            <Image src={imgGreenMarine} alt="green marine" />{" "}
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">Residence Green Marine</h2>
+      <div className="prose text-center mx-auto my-12 mt-24">
+        <h1 className="text-5xl">Holiday Services</h1>
+        <p className="">
+          Affittiamo e vendiamo i migliori residence di Silvi Marina
+        </p>
+      </div>
+      <div className="flex flex-wrap justify-center gap-10">
+        <div className="card w-96  card-compact sm:card-normal bg-base-100 shadow-xl">
+          <Link href={"/green-marine"}>
+            <figure>
+              <Image src={imgGreenMarine} alt="green marine" />{" "}
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Residence Green Marine</h2>
 
-            <p>
-              Appartamenti Vacanze a Silvi Marina Affitti appartamenti in
-              residence sul mare a Silvi Marina. E &apos; uno dei fiori
-              all&apos; occhiello di Silvi Marina. Unico per caratteristiche,
-              con un parco di migliaia…
-            </p>
-          </div>
-        </Link>
+              <p>
+                Appartamenti Vacanze a Silvi Marina Affitti appartamenti in
+                residence sul mare a Silvi Marina. E &apos; uno dei fiori
+                all&apos; occhiello di Silvi Marina. Unico per caratteristiche,
+                con un parco di migliaia…
+              </p>
+            </div>
+          </Link>
+        </div>
+        <div className="card w-96 card-compact sm:card-normal bg-base-100 shadow-xl">
+          <Link href={"/green-marine"}>
+            <figure>
+              <Image src={imgGreenMarine} alt="green marine" />{" "}
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Residence Green Marine</h2>
+
+              <p>
+                Appartamenti Vacanze a Silvi Marina Affitti appartamenti in
+                residence sul mare a Silvi Marina. E &apos; uno dei fiori
+                all&apos; occhiello di Silvi Marina. Unico per caratteristiche,
+                con un parco di migliaia…
+              </p>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
